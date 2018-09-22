6a829f81261eae94c7b307634dab927124d5d19e
Harson89 <harson8_harson9@hotmail.com>
2018-09-22 02:26:13 +0200

    Napravljen ispis slika za recepte i sitni edit za svaku kategoriju

--- glavnaJela.php
+++ glavnaJela.php
@@ -3,6 +3,16 @@ session_start();
 
 require_once 'konekcija.php';
 
+$tipusera = $_SESSION['tip'];
+
+if ($tipusera == 1) {
+    require_once 'navbar.html';
+}
+
+else {
+    require_once 'navbargest.html';
+}
+
 /*Bere id usera koji imaju recept u toj kategoriji
 $stmt = $pdo->prepare('SELECT user_id FROM recepti WHERE kategorije_id= 1');
 $stmt->execute();
@@ -31,20 +41,36 @@ $query = "SELECT * FROM recepti WHERE kategorije_id = 1";
 $stmt2 = $pdo->prepare($query); 
 $stmt2->execute();
 while ($row = $stmt2->fetch()) {
-  
-    
-  
-  echo '    <table border=1px class="tg"> ';
+   
+   echo '<br> <br> <br>'; 
+   
+  echo '<div class="tabelica1">'; 
+  echo '    <table class="tabelica" border=1px > ';
   echo ' <tr>';
   echo ' <th colspan="2"> '.$row["naziv"].'</th> ';
   echo ' </tr> ';
   echo ' <tr> ';
-  echo '  <td rowspan="2"> "Slika" </td> ' ;
+  echo '  <td rowspan="2"> ';
+  
+  $idrecepta = $row ['id'];
+  
+  $stmt3 = $pdo->prepare('SELECT url FROM slike WHERE recepti_id = ?');
+$stmt3->execute([$idrecepta]);
+while($user = $stmt3->fetch()) {
+    
+    $url = $user['url'];
+    
+    echo "<img src='".$url."' alt='Nema slike'>";
+}
+  
+  
+  echo '</td> ' ;
+  
+  
   echo '<td> ';
-
   
   $vlasnik = $row['user_id'];
-  
+ 
 //Preko dobijenog id-a ispisuje ime i prezime usera 
 $stmt1 = $pdo->prepare('SELECT * FROM users WHERE id = ?');
 $stmt1->execute([$vlasnik]);
@@ -56,19 +82,26 @@ while($user = $stmt1->fetch()) {
    echo $imena . " " . $prezimena . "<br>";    
 }
 
-
-   
+  
+  
+  
   echo '</td> ';
   echo '</tr> ';
   echo ' <tr> ';
-  echo ' <td > </td> ';
+ 
   echo ' </tr> ';
   echo '<tr> ';
-  echo '  <td colspan="2"> '.$row["priprema"].'</td> ';
+  echo '  <td colspan="2" height="200px"> '.$row["priprema"].'</td> ';
   echo ' </tr>';
-  echo ' </table> '
+  echo '<tr>';
+  echo '</tr>';
+  echo ' </table> ';
+  echo '<br>';
+  echo '</div>'; 
 
-    ;
 }
 ?>
 
+<head>
+    <link rel="stylesheet" href="editzasvakukategoriju.css"
+</head>
