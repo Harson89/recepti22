b8b5d26a61bb01063ae25bebf2dc6bd2ada415a8
Harson89 <harson8_harson9@hotmail.com>
2018-09-24 12:07:16 +0200

    Napravljeno dodavanje slike u recept

--- dodavanjerecepata.php
+++ dodavanjerecepata.php
@@ -9,11 +9,12 @@ require_once 'dodavanjerecepata.html';
 
 
 
- if(isset($_GET['dodaj'])){
+ if(isset($_POST['dodaj'])){
 
-
+//print_r($_FILES); die();
 $target_dir = "slike_recepata/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$date_tmp = date('Ymdhis');
+$target_file = $target_dir . $date_tmp. basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
@@ -21,24 +22,28 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
+    //echo $uploadOk; die();
 }
 
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
+    //echo $uploadOk; die();
     
 } else {
+    //echo 'sad'; die();
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
+        echo $uploadOk; die();
     }
 }
 
      
      $autor = $_SESSION['id_usera'];
-     $naziv_recepta = ($_GET['nazivrecepta']);
-     $kategorija= ($_GET['kategorija']);
-     $priprema= ($_GET['priprema']);
+     $naziv_recepta = ($_POST['nazivrecepta']);
+     $kategorija= ($_POST['kategorija']);
+     $priprema= ($_POST['priprema']);
      
     /* 
      
@@ -55,9 +60,9 @@ if ($uploadOk == 0) {
      //Prebire id kategorije po odabranoj opciji
      
      
-     $stmt = $pdo->prepare('SELECT id FROM kategorije WHERE naziv = :kategorija');
-     $stmt->execute(['kategorija' => $kategorija]);
-     $user = $stmt->fetch();
+     $stmt = $pdo->prepare('SELECT id FROM kategorije WHERE naziv = ?');
+     $stmt->execute([$kategorija]);
+     $kat = $stmt->fetch();
      
     
      
@@ -75,7 +80,7 @@ if ($uploadOk == 0) {
      $query = "INSERT INTO recepti(kategorije_id,user_id,naziv, priprema , slika) "
              . "VALUES (?,?,?,?,?)";
      $stmt = $pdo->prepare($query);
-     $stmt->execute([$user['id'],$autor,$naziv_recepta,$priprema,$target_file]);
+     $stmt->execute([$kat['id'],$autor,$naziv_recepta,$priprema,$target_file]);
      
       // Nekad radilo -->   header('Location: /recepti22/kategorije.php');
 
